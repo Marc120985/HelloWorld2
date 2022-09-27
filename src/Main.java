3cbdd36b59a27862c9bef0838fdd9cab8a57a8f6
Marc120985 <creativ.zeit@gmail.com>
2022-09-27 15:54:10 +0300

    Add method

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+
+        String name = "Das ist voll fancy";
+
         System.out.println("This ist now Version 3");
         System.out.println("Dennis war hier");
         System.out.println("Steven has been here ;-)");
 
         System.out.println("The Sum is: " + addition(9, 8));
         System.out.println(addString(3, 4));
+
+        if(name.length() < 10){
+            System.out.println("Es sind weniger als 10 Zeichen");
+        }else {
+            System.out.println("Es sind mehr als 10 Zeichen");
+        }
+
+        System.out.println(name.contains("fancy"));
     }
 
     public static int addition(int number1, int number2){
