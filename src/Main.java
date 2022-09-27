c697aa1e3c017a07fe1802debd9361909839519c
Marc120985 <creativ.zeit@gmail.com>
2022-09-27 11:14:27 +0300

    Initial Version 2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Das ist ein Versuch");
         System.out.println("Das ist ein neuer Versuch");
+        System.out.println("Das ist Version 2");
     }
 }
