80aae94780c3320b4cca47c80d0b938dd1cc4ff7
Marc120985 <creativ.zeit@gmail.com>
2022-09-27 15:09:26 +0300

    Add Methoden

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Das ist ein Versuch");
-        System.out.println("Das ist ein neuer Versuch");
-        System.out.println("Das ist Version 2");
         System.out.println("This ist now Version 3");
         System.out.println("Dennis war hier");
-
         System.out.println("Steven has been here ;-)");
+
+        System.out.println("The Sum is: " + addition(9, 8));
+        System.out.println(addString(3, 4));
+    }
+
+    public static int addition(int number1, int number2){
+        return  number1 + number2;
+    }
+
+    public static String addString(int number3, int number4) {
+        return "Summe: " + (number3 + number4);
     }
 }
